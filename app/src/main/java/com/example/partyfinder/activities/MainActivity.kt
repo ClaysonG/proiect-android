be0e152d22358a08b647cd849c51e14abd29ed2e
Victor David <victordavid2506@gmail.com>
2023-06-08 20:13:07 +0300

    changed project structure + custom font

---
+++ app/src/main/java/com/example/partyfinder/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.partyfinder.activities
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import com.example.partyfinder.R
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
